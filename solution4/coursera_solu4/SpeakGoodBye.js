f1aa1f901b52b6d576d6ffebc20bb6ee16196518
Mujeeb <mujeebajala@gmail.com>
2022-09-10 03:56:31 +0100

    any message

---
+++ solution4/coursera_solu4/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+
+(function () {
+  var byeSpeaker = {}
+
+  var speakWord = "Good Bye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker
+
+})();
---
+++ solution4/coursera_solu4/SpeakHello.js
@@ -0,0 +1,13 @@
+
+(function () {
+  var helloSpeaker = {}
+
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.helloSpeaker = helloSpeaker
+
+})();
